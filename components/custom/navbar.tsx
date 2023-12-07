8c5f347d1254614c72ab5e7b1c1f239a428626ae
deanadriannn <deanadrian644@gmail.com>
2023-12-07 16:32:55 +0700

    create navbar

---
+++ components/custom/navbar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: "600",
+  subsets: ['latin']
+})
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#1E1E1D] flex justify-between w-full md:px-5 lg:px-36 xl:px-52 2xl:px-72">
+      <Image 
+        src="/assets/logo.svg"
+        alt="logo"
+        height={80}
+        width={80}
+      />
+      <div className={cn("flex justify-between items-center gap-16", plusJakartaSans.className)}>
+        <p>Home</p>
+        <p>Projects</p>
+        <p>Experience</p>
+        <p>Contact</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
